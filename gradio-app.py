aee14e0cfa52156adfe700d8c2c8292ad693f1d4
EliasVincent <riseyeti@outlook.de>
2023-02-25 13:51:19 +0100

    embed subs

--- gradio-app.py
+++ gradio-app.py
@@ -1,12 +1,12 @@
 import gradio as gr
-import ffmpeg as ff
+import ffmpeg
 import os
 import tempfile
 import torch
 import whisper
 from whisper.utils import get_writer
 
-def transcribe(inputFile, language, model, task):
+def transcribe(inputFile, language, model, task, addSrtToVideo):
     print("gpu available: " + str(torch.cuda.is_available()))
     gpu = torch.cuda.is_available()
     model = whisper.load_model(model)
@@ -22,6 +22,24 @@ def transcribe(inputFile, language, model, task):
     writer(whisperOutput, inputFile.name)
     srtFile = f"{inputFile.name}" + ".srt"
     
+    if addSrtToVideo:
+        video_out = inputFile.name + "_output.mkv"
+
+        input_ffmpeg = ffmpeg.input(inputFile.name)
+        input_ffmpeg_sub = ffmpeg.input(srtFile)
+
+        input_video = input_ffmpeg['v']
+        input_audio = input_ffmpeg['a']
+        input_subtitles = input_ffmpeg_sub['s']
+        stream = ffmpeg.output(
+            input_video, input_audio, input_subtitles, video_out,
+            vcodec='copy', acodec='copy', scodec='srt'
+        )
+        stream = ffmpeg.overwrite_output(stream)
+        #execute(stream, desc=f"Adding `SubRip Subtitle file` to {inputFile.name}")
+        ffmpeg.run(stream)
+        return video_out
+    
     return srtFile
 
 demo = gr.Interface(
@@ -30,7 +48,8 @@ demo = gr.Interface(
         "file",
         gr.Textbox(value="source language (en, de, ja, ..)"),
         gr.Dropdown(["tiny", "small", "medium", "large"], value="tiny"), 
-        gr.Radio(["transcribe", "translate"], value="translate")
+        gr.Radio(["transcribe", "translate"], value="translate"),
+        gr.Checkbox(label="embed subtitles into video file")
     ],
     outputs="file"
 )
